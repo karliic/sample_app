794ea92f6086928a3c5ed730dc3386b2f38bbcb8
Karlis <karlis@karlis-VirtualBox.(none)>
2013-03-02 21:20:27 +0200

    Edit text

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
     else
-      flash.now[:error] = 'Invalid email/password combination'
+      flash.now[:error] = 'Invalid email/password'
       render 'new'
     end
   end
